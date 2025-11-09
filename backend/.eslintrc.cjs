9b5192b3028234612cf58d01fd08dac1729e666b
Siddharth Singodia <siddharthsingodia2643@gmail.com>
2025-11-09 13:29:12 +0530

    chore(backend): scaffold express + typescript + prisma config

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2020, sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: { 'no-console': 'off' }
+};
